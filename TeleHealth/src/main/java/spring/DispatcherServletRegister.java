8db15778ce4b74fd5a9150618eb80a1b9bea75c7
billy <billy@DESKTOP-K18VE76>
2018-03-04 23:17:27 +0800

    第一次 COMMIT
    初始化

---
+++ TeleHealth/src/main/java/spring/DispatcherServletRegister.java
@@ -0,0 +1,20 @@
+package spring;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherServletRegister extends AbstractAnnotationConfigDispatcherServletInitializer {
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {spring.SpringMVCJavaConfiguration.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"*.controller"};
+	}
+	
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+}
