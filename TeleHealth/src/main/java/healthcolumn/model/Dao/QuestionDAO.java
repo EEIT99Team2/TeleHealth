7fb70b655e4a10c6d5216ceadf0327b131e703bb
黃琮文 <Student@192.168.21.33>
2018-03-08 20:23:12 +0800

    20180308 20:22 commit
    健康專欄,留言

--- TeleHealth/src/main/java/healthcolumn/model/Dao/QuestionDAO.java
+++ TeleHealth/src/main/java/healthcolumn/model/Dao/QuestionDAO.java
@@ -21,9 +21,9 @@ public class QuestionDAO {
 	//選擇文章所有回應的文
 	public List<QuestionBean>  selectresponse(String title) {
 		NativeQuery query=this.getSession().createNativeQuery
-				("select emp.empName,mem.memName, que.Content,que.createDate,que.QAtype \r\n" + 
+				("select emp.empName,mem.memName, que.Content,que.createDate,que.modifyTime,que.QAtype \r\n" + 
 						"from question que\r\n" + 
-						"   join healthColumn hel on hel.columnId=que.QAcolumnId  \r\n" + 
+						"   join healthColumn hel on hel.title=que.quetitle  \r\n" + 
 						"  left outer  join employees emp on que.empId=emp.empId \r\n" + 
 						"  left outer  join members mem on que.memberId=mem.memberId\r\n" + 
 						" where hel.title=?");
@@ -34,20 +34,20 @@ public class QuestionDAO {
 	//選擇護理師文章所有回應的文
 		public List<QuestionBean>  selectresponseEmp(String empId) {
 			NativeQuery query=this.getSession().createNativeQuery
-					("  select QAcolumnId,QAtype,Content,createDate,modifyTime from question where empId=?");
+					("select * from question where empId=?");
 			query.setParameter(1, empId);
 			List<QuestionBean> data=(List<QuestionBean>)query.list();
 			return data;		
 		}
-	//選擇護理師文章所有回應的文
+	//選擇會員文章所有回應的文
 				public List<QuestionBean>  selectresponseMem(String memId) {
 					NativeQuery query=this.getSession().createNativeQuery
-							("   select QAcolumnId,QAtype,Content,createDate,modifyTime from question  where memberId=?");
+							("select * from question  where memberId=?");
 					query.setParameter(1, memId);
 					List<QuestionBean> data=(List<QuestionBean>)query.list();
 					return data;		
 				}
-	//增加	
+		//增加	
 		public QuestionBean insertQA(QuestionBean bean) {
 			if(bean!=null) {
 				QuestionBean temp =this.getSession().get(QuestionBean.class, bean.getId());
@@ -59,19 +59,29 @@ public class QuestionDAO {
 			return null;
 		}
 
-		//修改		
-			public QuestionBean update(int Id,String memberId,Date createDate,String advisorycode,String Content) {
+		//修改會員po文		
+			public QuestionBean updateMem(int Id,String memberId,String Content) {
 			QuestionBean result = this.getSession().get(QuestionBean.class, Id);
 			if(result!=null) {
 				result.setId(Id);
 				result.setMemberId(memberId);
-				result.setCreateDate(createDate);
-				result.setAdvisorycode(advisorycode);
+				result.setModifyTime(new Date());				
 				result.setContent(Content);}
 			return result;
 		}
-		//刪除
-		public boolean delete(int Id,String memberId) {
+		//修改員工po文		
+			public QuestionBean updateEmp(int Id,String empId,String Content) {
+			QuestionBean result = this.getSession().get(QuestionBean.class, Id);
+			if(result!=null) {
+				result.setId(Id);
+				result.setEmpId(empId);
+				result.setModifyTime(new Date());				
+				result.setContent(Content);}
+			return result;
+		}	
+			
+		//刪除員工文章
+		public boolean deleteMem(int Id,String memberId) {
 			QuestionBean result = this.getSession().get(QuestionBean.class, memberId);
 			if(result!=null) {
 				this.getSession().delete(result);
@@ -79,5 +89,13 @@ public class QuestionDAO {
 			}
 			return false;
 		}
-
+		//刪除會員文章
+		public boolean deletEmp(int id,String EmpId) {
+			QuestionBean result = this.getSession().get(QuestionBean.class, EmpId);
+			if(result!=null) {
+				this.getSession().delete(result);
+				return true;
+			}
+			return false;
+		}
 }
