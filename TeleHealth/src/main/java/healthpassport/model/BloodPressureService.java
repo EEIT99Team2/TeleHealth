1d698333a1a7e708ce066f8e10e7a4b601270882
陳良煒 <i13b3221574@gmail.com>
2018-03-18 15:18:04 +0800

    20180318 05.24
    15歲以下 血壓修改中

--- TeleHealth/src/main/java/healthpassport/model/BloodPressureService.java
+++ TeleHealth/src/main/java/healthpassport/model/BloodPressureService.java
@@ -21,86 +21,107 @@ public class BloodPressureService {
 	String maxBloodPressure = "BloodPressureSystole";
 	String Heartbeat = "Heartbeat";
 
-	public BloodPressureBean insert(BloodPressureBean bean,String gender,Integer age) {
-		//client血壓值
+	public BloodPressureBean insert(BloodPressureBean bean, String gender, Integer age) {
+		// client血壓值
 		Integer cminBP = bean.getMinBloodPressure();
 		Integer cmaxBP = bean.getMaxBloodPressure();
 		Integer cIHB = bean.getHeartBeat();
 		Integer newHB = new Integer(cIHB);
 		Double HB = newHB.doubleValue();
-		
-		DataAnalysisBean Diastole =null;
-		DataAnalysisBean Systole =null;
-		DataAnalysisBean HeartBeat =null;
-		if(gender.equals("M")) {
-			Diastole = dataAnalysisDao.selectGroupId(minBloodPressure, gender, age);
-			Systole = dataAnalysisDao.selectGroupId(maxBloodPressure, gender, age);
-			HeartBeat = dataAnalysisDao.Up18HBandBS(Heartbeat, gender, age,HB);
-		}else if(gender.equals("F")){
-			Diastole = dataAnalysisDao.selectGroupId(minBloodPressure, gender, age);
-			Systole = dataAnalysisDao.selectGroupId(maxBloodPressure, gender, age);
-			HeartBeat = dataAnalysisDao.Up18HBandBS(Heartbeat, gender, age,HB);
-		}
-		//收縮壓
-		Double minSystole =  Systole.getMinvalue();
-		Double maxSystole =  Systole.getMaxvalue();
-		String SystoleResult = Systole.getResult();
-		if(cmaxBP<minSystole) {
-			SystoleResult="偏低";
-		}else if(cmaxBP>maxSystole){
-			SystoleResult="偏高";
-		}
-		//舒張壓
-		Double minDiastole =  Diastole.getMinvalue();
-		Double maxDiastole =  Diastole.getMaxvalue();
-		String DiastoleResult = Diastole.getResult();
-		if(cminBP<minDiastole) {
-			DiastoleResult="偏低";
-		}else if(cminBP>maxDiastole){
-			DiastoleResult ="偏高";
-		}		
-		//心跳值
+		DataAnalysisBean Diastole = null;
+		DataAnalysisBean Systole = null;
+		DataAnalysisBean HeartBeat = null;
+		String DiastoleResult=null;//舒張壓結果
+		String SystoleResult=null;//收縮壓結果
 		String HeartBeatResult = null;
-		if(HB<50) {
-			HeartBeatResult="太慢";
-		}else if(HB>110){
-			HeartBeatResult="超快";
-		}else {
-			HeartBeatResult =HeartBeat.getResult();
+		if (age < 15) {
+			if(cminBP<30) {
+				DiastoleResult="低於正常值";
+			}else if(cminBP>59){
+				DiastoleResult="高於正常值";
+			}else {
+				DiastoleResult="正常";
+			}
+			if(cmaxBP<70) {
+				SystoleResult="低於正常值";
+			}else if(cmaxBP>100) {
+				SystoleResult="高於正常值";
+			}else {
+				SystoleResult="正常";
+			}
+			if (HB < 50) {
+				HeartBeatResult = "太慢";
+			} else if (HB > 110) {
+				HeartBeatResult = "超快";
+			} else {
+				HeartBeatResult ="正常標準值";
+			}
+		} else if (age >= 15) {
+			Diastole = dataAnalysisDao.selectGroupId(minBloodPressure, gender, age);
+			Systole = dataAnalysisDao.selectGroupId(maxBloodPressure, gender, age);
+			HeartBeat = dataAnalysisDao.Up18HBandBS(Heartbeat, gender, age, HB);
+			Double minSystole = Systole.getMinvalue();
+			Double maxSystole = Systole.getMaxvalue();
+			// 收縮壓
+			SystoleResult = Systole.getResult();
+			if (cmaxBP < minSystole) {
+				SystoleResult = "偏低";
+			} else if (cmaxBP > maxSystole) {
+				SystoleResult = "偏高";
+			}
+			// 舒張壓
+			Double minDiastole = Diastole.getMinvalue();
+			Double maxDiastole = Diastole.getMaxvalue();
+			DiastoleResult = Diastole.getResult();
+			if (cminBP < minDiastole) {
+				DiastoleResult = "偏低";
+			} else if (cminBP > maxDiastole) {
+				DiastoleResult = "偏高";
+			}
+			// 心跳值
+			if (HB < 50) {
+				HeartBeatResult = "太慢";
+			} else if (HB > 110) {
+				HeartBeatResult = "超快";
+			} else {
+				HeartBeatResult = HeartBeat.getResult();
+			}
 		}
 		bean.getMinBloodPressure();
 		bean.getMaxBloodPressure();
 		bean.getHeartBeat();
-		bean.setResult("收縮壓:"+SystoleResult+","+"舒張壓:"+DiastoleResult+","+"脈搏:"+HeartBeatResult);
+		bean.setResult("收縮壓:" + SystoleResult + "," + "舒張壓:" + DiastoleResult + "," + "脈搏:" + HeartBeatResult);
 		bean.setCreateTime(new java.util.Date());
-		BloodPressureBean result = bloodPressureDao.insert(bean);		
+		BloodPressureBean result = bloodPressureDao.insert(bean);
 		return result;
 	}
-	
-	public List<BloodPressureBean> selectMemberid(String memberid) {		
+
+	public List<BloodPressureBean> selectMemberid(String memberid) {
 		List<BloodPressureBean> result = bloodPressureDao.selectMemberId(memberid);
 		return result;
 	}
-	
+
 	public BloodPressureBean newOne(String memberid) {
 		BloodPressureBean result = bloodPressureDao.topOneData(memberid);
 		return result;
 	}
-	
-	//7
+
+	// 7
 	public List<BloodPressureBean> selectMemberidseven(String memberid) {
 		List<BloodPressureBean> result = bloodPressureDao.sevenData(memberid);
 		return result;
 	}
-	//30
+
+	// 30
 	public List<BloodPressureBean> selectMemberidthirty(String memberid) {
 		List<BloodPressureBean> result = bloodPressureDao.thirtyData(memberid);
 		return result;
 	}
-	//180
+
+	// 180
 	public List<BloodPressureBean> selectMemberidthreemon(String memberid) {
 		List<BloodPressureBean> result = bloodPressureDao.thrmonthData(memberid);
 		return result;
 	}
-	
+
 }
--- TeleHealth/src/main/java/healthpassport/model/DataAnalysisService.java
+++ TeleHealth/src/main/java/healthpassport/model/DataAnalysisService.java
@@ -1,13 +1,10 @@
 package healthpassport.model;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import healthpassport.model.dao.DataAnalysisDAO;
-
-
 
 @Service
 @Transactional
@@ -15,17 +12,35 @@ public class DataAnalysisService {
 	@Autowired
 	private DataAnalysisDAO dataanalysisDao;
 	
-	
 //	@Transactional(readOnly=true)
 	public List<DataAnalysisBean> selectDataAll() {
 		List<DataAnalysisBean> result = dataanalysisDao.selectDataAll();
 		return result;
 	}
 //血壓,舒張壓
+	Double minvalue = null;
+	Double maxvalue = null;
 	public DataAnalysisBean selectGroupId(String groupid,String gender,Integer age) {
-		DataAnalysisBean dataAnalysis = dataanalysisDao.selectGroupId(groupid, gender, age);
-		return  dataAnalysis;
+	if(age<15) {
+		if(groupid.equals("BloodPressureDiastole")) {
+			DataAnalysisBean dataAnalysis = dataanalysisDao.selectGroupId(groupid, gender, 15);
+			minvalue = dataAnalysis.getMinvalue()-29;
+			maxvalue = dataAnalysis.getMaxvalue()-16;
+			dataAnalysis.setMinvalue(minvalue);
+			dataAnalysis.setMaxvalue(maxvalue);
+			return dataAnalysis;
+		}else if(groupid.equals("BloodPressureSystole")) {
+			DataAnalysisBean dataAnalysis = dataanalysisDao.selectGroupId(groupid, gender, 15);
+			minvalue = dataAnalysis.getMinvalue()-33;
+			maxvalue = dataAnalysis.getMaxvalue()-26;
+			dataAnalysis.setMinvalue(minvalue);
+			dataAnalysis.setMaxvalue(maxvalue);
+			return dataAnalysis;
+		}		
 	}
+	DataAnalysisBean dataAnalysis = dataanalysisDao.selectGroupId(groupid, gender, age);
+	return dataAnalysis;
+}
 	public DataAnalysisBean selectdataBs(String gender) {
 		DataAnalysisBean dataanaly = dataanalysisDao.BSdata(gender);
 		return dataanaly;
